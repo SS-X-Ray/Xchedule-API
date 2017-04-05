2e17167b0803ad2a2d0b0d3eda7e850d9bd67162
LeoLee <leo.li@iss.nthu.edu.tw>
2017-04-05 23:03:35 +0800

    ADD model/schedule

---
+++ models/schedule.rb
@@ -0,0 +1,50 @@
+require 'json'
+require 'base64'
+require 'rbnacl/libsodium'
+
+# Holds a full configuration document information
+class Schedule
+  STORE_DIR = 'db/'.freeze
+
+  attr_accessor :id, :time
+
+  def initialize(new_user_schedule)
+    @id = new_user_schedule['id'] || new_id
+    @time = new_user_schedule['time']
+  end
+
+  def new_id
+    Base64.urlsafe_encode64(RbNaCl::Hash.sha256(Time.now.to_s))[0..9]
+  end
+
+  def to_json(options = {})
+    JSON({ id: @id,
+           time: @time },
+         options)
+  end
+
+  def save
+    File.open(STORE_DIR + @id + '.txt', 'w') do |file|
+      file.write(to_json)
+    end
+
+    true
+  rescue
+    false
+  end
+
+  def self.find(find_id)
+    config_file = File.read(STORE_DIR + find_id + '.txt')
+    Configuration.new JSON.parse(config_file)
+  end
+
+  def self.all
+    Dir.glob(STORE_DIR + '*.txt').map do |filename|
+      filename.match(/#{Regexp.quote(STORE_DIR)}(.*)\.txt/)[1]
+    end
+  end
+
+  def self.setup
+    Dir.mkdir(Schedule::STORE_DIR) unless Dir.exist? STORE_DIR
+  end
+end
