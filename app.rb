fb429783e001af61d0a5c9114ae0dc4e161b7ece
Martin Hsia <marandy0905@gmail.com>
2017-04-13 02:57:11 +0800

    Route updated

--- app.rb
+++ app.rb
@@ -11,41 +11,78 @@ class XcheduleAPI < Sinatra::Base
     'Xchedule web API up at /api/v1'
   end
 
-  get "/#{API_VER}/schedule/" do
-    content_type 'application/json'
-
-    output = { Schedule_id: Schedule.all }
-    JSON.pretty_generate(output)
-  end
-
-  get "/#{API_VER}/schedule/:id.json" do
+  get "/#{API_VER}/activity/:id" do
     content_type 'application/json'
 
     begin
-      output = { Schedule: Schedule.find(params[:id]) }
+      activity = Activity.find(id: params[:id])
+      output = { Name: activity.name, Location: activity.location,
+                 Possible_Time: activity.possible_time,
+                 Result_Time: activity.result_time }
       JSON.pretty_generate(output)
     rescue => e
-      logger.info "FAILED to GET Schedule: #{e.inspect}"
+      logger.info "FAILED to GET Activity: #{e.inspect}"
       status 404
     end
   end
 
-  post "/#{API_VER}/schedule/?" do
+  post "/#{API_VER}/activity/?" do
     content_type 'application/json'
 
     begin
       new_data = JSON.parse(request.body.read)
-      new_timeseries = Schedule.new(new_data)
-      if new_timeseries.save
-        logger.info "NEW Schedule STORED: #{new_timeseries.id}"
-      else
-        halt 400, "Could not store timeseries: #{new_timeseries}"
-      end
-
-      redirect "/#{API_VER}/schedule/" + new_timeseries.id.to_s + '.json'
+      Activity.create(new_data)
     rescue => e
-      logger.info "FAILED to create new timeseries: #{e.inspect}"
+      logger.info "FAILED to create new Activity: #{e.inspect}"
       status 400
     end
+    status 201
+  end
+
+  patch "/#{API_VER}/activity/?" do
+    content_type 'application/json'
+
+    begin
+      update_data = JSON.parse(request.body.read)
+      old_record = Activity.find(id: update_data[:id])
+
+      update_data.keys.each do |key|
+        if key == :id
+          continue
+        else
+          old_record.send("#{key}=", update_data[key])
+          old_record.save
+        end
+      end
+    rescue => e
+      logger.info "FAILED to update partial Activity info: #{e.inspect}"
+      status 400
+    end
+  end
+
+  get "/#{API_VER}/participant/:activity_id" do
+    content_type 'application/json'
+
+    begin
+      participant = Participant.find(activity_id: params[:activity_id])
+      output = { User_id: participant.user_id }
+      JSON.pretty_generate(output)
+    rescue => e
+      logger.info "FAILED to GET Participant: #{e.inspect}"
+      status 404
+    end
+  end
+
+  post "/#{API_VER}/participant/?" do
+    content_type 'application/json'
+
+    begin
+      new_data = JSON.parse(request.body.read)
+      Participant.create(new_data)
+    rescue => e
+      logger.info "FAILED to create new Activity: #{e.inspect}"
+      status 400
+    end
+    status 201
   end
 end
