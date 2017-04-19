d2ca6701ed2a55ef3733e01326c78ed12e35f14f
Martin Hsia <marandy0905@gmail.com>
2017-04-19 19:58:20 +0800

    bug fix

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -50,14 +50,14 @@ describe 'Testing Activity resource routes' do
     it 'HAPPY: should add result time to existing activity' do
       new_activity = Activity.create(name: 'meeting', location: 'CTM321')
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: new_activity.id, result_time: 1 }.to_json
+      req_body = { activity_id: new_activity.id, result_time: '1' }.to_json
       patch '/api/v1/activity/', req_body, req_header
       _(last_response.status).must_equal 201
     end
 
     it 'SAD: should not add result time to non-existing activity' do
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: "#{invalid_id(Activity)}", result_time: 1 }.to_json
+      req_body = { activity_id: "#{invalid_id(Activity)}", result_time: '1' }.to_json
       patch '/api/v1/activity/', req_body, req_header
       _(last_response.status).must_equal 400
     end
