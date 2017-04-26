2d837caa52aa826768fd9d472e68dba32b9b641d
Martin Hsia <marandy0905@gmail.com>
2017-04-26 21:17:10 +0800

    spec updated for service object and account db

--- spec/participant_spec.rb
+++ spec/participant_spec.rb
@@ -5,6 +5,12 @@ describe 'Testing Participant resource routes' do
   before do
     Activity.dataset.delete
     Participant.dataset.delete
+
+    @original_password = 'mypassword'
+    @acc1 = CreateAccount.call(username: 'xray', email: 'xray@nthu.edu.tw',
+                               password: @original_password)
+    @acc2 = CreateAccount.call(username: 'xray2', email: 'xray2@nthu.edu.tw',
+                               password: @original_password)
   end
 
   describe 'Creating new participants for activity' do
@@ -12,34 +18,32 @@ describe 'Testing Participant resource routes' do
       existing_activity = Activity.create(name: 'meeting')
 
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: existing_activity.id, user_id: 1 }.to_json
-      post '/api/v1/participant/?', req_body, req_header
+      req_body = { activity_id: existing_activity.id, participant_id: @acc1.id }.to_json
+      post "/#{API_VER}/activity/participant/?", req_body, req_header
       _(last_response.status).must_equal 201
     end
 
     it 'SAD: should not add a participant for non-existant activity' do
       req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: "#{invalid_id(Activity)}", user_id: 1 }.to_json
-      post '/api/v1/participant/?', req_body, req_header
+      req_body = { activity_id: "#{invalid_id(Activity)}", participant_id: @acc1.id }.to_json
+      post "/#{API_VER}/activity/participant/?", req_body, req_header
       _(last_response.status).must_equal 400
       _(last_response.location).must_be_nil
     end
   end
 
-  describe 'Getting participants' do
-    it 'HAPPY: should find existing participant' do
-      activity =  Activity.create(name: 'meeting')
-      participant = activity.add_participant(user_id: 1)
-
-      get "api/v1/participant/#{participant.activity_id}"
+  describe 'Getting participate activities' do
+    it 'HAPPY: should find all activies a account participate' do
+      activity = Activity.create(name: 'meeting')
+      AddParticipantToActivity.call(activity_id: activity.id, participant_id: @acc1.id)
+      get "/#{API_VER}/account/participants/#{@acc1.id}"
       _(last_response.status).must_equal 200
-      parsed_participant = JSON.parse(last_response.body)
-      _(parsed_participant['User_ids'][0]).must_equal participant.user_id
+      parsed_activities = JSON.parse(last_response.body)
+      _(parsed_activities['activities'][0]).must_equal activity.id
     end
 
-    it 'SAD: should not find non-existant activity and participant' do
-      activity_id = invalid_id(Activity)
-      get "api/v1/participant/#{activity_id}"
+    it 'SAD: should not find activities for not existing account' do
+      get "/#{API_VER}/account/participants/#{invalid_id(@acc1.id)}"
       _(last_response.status).must_equal 404
     end
   end
