5a8db45bdcff4e2e37c57c9bfa9a18be62037736
Martin Hsia <marandy0905@gmail.com>
2017-04-26 21:17:10 +0800

    spec updated for service object and account db

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -2,64 +2,27 @@ require_relative './spec_helper'
 
 describe 'Testing Activity resource routes' do
   before do
-    Activity.dataset.delete
-    Participant.dataset.delete
+    Account.dataset.destroy
+    Activity.dataset.destroy
+
+    @original_password = 'mypassword'
+    @acc1 = CreateAccount.call(username: 'xray', email: 'xray@nthu.edu.tw',
+                               password: @original_password)
+    @acc2 = CreateAccount.call(username: 'xray2', email: 'xray2@nthu.edu.tw',
+                               password: @original_password)
   end
 
-  describe 'Can create a new Activity' do
-    it 'HAPPY: should create a new unique Activity' do
-      req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { name: 'Meeting', location: 'CTM321' }.to_json
-      post '/api/v1/activity/', req_body, req_header
-      _(last_response.status).must_equal 201
-    end
-  end
-
-  describe 'Can find existing activity' do
-    it 'HAPPY: should find an existing activity' do
-      new_activity = Activity.create(name: 'meeting', location: 'CTM321')
-      get "/api/v1/activity/#{new_activity.id}"
-      _(last_response.status).must_equal 200
-
-      results = JSON.parse(last_response.body)
-      _(results['Name']).must_equal new_activity.name
+  describe 'Can organize/add participant a new Activity via a account' do
+    it 'should organize one new Activity' do
+      @e1 = CreateActivityForOrganizer.call(organizer_id: @acc1.id,
+                                            name: 'meeting',
+                                            location: 'CTM321')
+      @e1.organizer_id.must_equal @acc1.id
     end
 
-    it 'SAD: should not find non-existent activity' do
-      get "/api/v1/activity/#{invalid_id(Activity)}"
-      _(last_response.status).must_equal 404
-    end
-  end
-
-  describe 'Adding possible time and result time' do
-    it 'HAPPY: should add a list of possible time to existing activity' do
-      new_activity = Activity.create(name: 'meeting', location: 'CTM321')
-      req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: new_activity.id, possible_time: '[1, 2, 3]' }.to_json
-      patch '/api/v1/activity/', req_body, req_header
-      _(last_response.status).must_equal 201
-    end
-
-    it 'SAD: should not add a list of possible_time to non-existing activity' do
-      req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: "#{invalid_id(Activity)}", possible_time: '[1, 2, 3]' }.to_json
-      patch '/api/v1/activity/', req_body, req_header
-      _(last_response.status).must_equal 400
-    end
-
-    it 'HAPPY: should add result time to existing activity' do
-      new_activity = Activity.create(name: 'meeting', location: 'CTM321')
-      req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: new_activity.id, result_time: '1' }.to_json
-      patch '/api/v1/activity/', req_body, req_header
-      _(last_response.status).must_equal 201
-    end
-
-    it 'SAD: should not add result time to non-existing activity' do
-      req_header = { 'CONTENT_TYPE' => 'application/json' }
-      req_body = { activity_id: "#{invalid_id(Activity)}", result_time: '1' }.to_json
-      patch '/api/v1/activity/', req_body, req_header
-      _(last_response.status).must_equal 400
+    it 'should add a participant to an existing Activity' do
+      @acc2.AddParticipantToActivity.call(Activity[id: @e1.id])
+      @acc2.activities.first.must_equal @e1
     end
   end
 end
