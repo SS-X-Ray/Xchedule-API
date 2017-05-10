84b55c2ce0d774e3d73e5fa283de4ef632eedca7
Martin Hsia <marandy0905@gmail.com>
2017-05-10 21:58:21 +0800

    Switching our API to forbid insecure HTTP access.

--- controllers/base.rb
+++ controllers/base.rb
@@ -13,6 +13,17 @@ class XcheduleAPI < Sinatra::Base
     SecureDB.setup(settings.config)
   end
 
+  def secure_request?
+    request.scheme.casecmp(settings.config.SECURE_SCHEME).zero?
+  end
+
+  before do
+    halt(403, 'Use HTTPS only') unless secure_request?
+
+    host_url = "#{settings.config.SECURE_SCHEME}://#{request.env['HTTP_HOST']}"
+    @request_url = URI.join(host_url, request.path.to_s)
+  end
+
   get '/?' do
     'Xchedule web API up at /api/v1'
   end
