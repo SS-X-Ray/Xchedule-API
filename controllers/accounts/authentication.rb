82842844c9d36fab08c3094e7d48f99550cbb89c
LeoLee <leo.li@iss.nthu.edu.tw>
2017-06-03 00:49:36 +0800

    fix

--- controllers/accounts/authentication.rb
+++ controllers/accounts/authentication.rb
@@ -14,4 +14,16 @@ class XcheduleAPI < Sinatra::Base
     puts "AUTH: #{authenticated}"
     authenticated ? authenticated.to_json : halt(403)
   end
+
+  get '/api/v1/google_account' do
+    content_type 'application/json'
+    begin
+      sso_account, auth_token = AuthenticateSsoAccount.new(settings.config).call(params['access_token'])
+      { account: sso_account, auth_token: auth_token }.to_json
+    rescue => e
+      puts e.inspect
+      logger.info "FAILED to validate Github account: #{e.inspect}"
+      halt 400
+    end
+  end
 end
